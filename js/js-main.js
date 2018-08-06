75282b6415445b31d4560c70f530edcbdd2f25cf
Mohamed Ibrahim <mohamed.mahmoud59@gmail.com>
2018-08-06 14:09:04 +0200

    Website test

---
+++ js/js-main.js
@@ -0,0 +1,31 @@
+  $( document ).ready(function() {
+    //   Navbar change When scroll
+    var scrollTop = 0;
+    $(window).scroll(function(){
+      scrollTop = $(window).scrollTop();      
+      if (scrollTop >= 100) {
+        $('.navbar-rubikal').addClass('scrolled-nav');
+        $('.navbar-rubikal .logo-rubikal img').addClass('scroll-logo');
+      } else if (scrollTop < 100) {
+        $('.navbar-rubikal').removeClass('scrolled-nav');
+        $('.navbar-rubikal .logo-rubikal img').removeClass('scroll-logo');
+
+      } 
+      
+    }); 
+      // Change li menu active
+      $('.navbar-rubikal .navbar-nav li a').click(function() {
+        $('li').removeClass();
+        $(this).parent().addClass('active');
+      });
+
+      // jumo to next section
+      $("#jump-section").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#services").offset().top
+        }, 2000);
+      });
+      if ($(window).width() > 1024) {
+          $('.head-rubikal .text-action').addClass('scale');
+      }
+  });
